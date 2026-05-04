cdeb17bb68cfb755ed61ebeb4bcd1afc8cd41b92
Maksim Umaev <yastey637@gmail.com>
2026-05-04 14:51:15 +0500

    make decrypt method

--- VigenereCipher/Service/CipherService.cs
+++ VigenereCipher/Service/CipherService.cs
@@ -9,6 +9,8 @@ namespace VigenereCipher.Service
     internal class CipherService : ICipherService
     {
         private readonly string _alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+
+        //Encrypt и Decrypt абсолютно одиннаковые, даже формулу в Encrypt можно использовать такую же как и в Decrypt. Нужен рефакторинг
         public string Encrypt(string message, string key)
         {
             var preparedMessage = prepareTextBeforeEncrypt(message);
@@ -17,13 +19,13 @@ namespace VigenereCipher.Service
             var cipher = new StringBuilder();
             for (var i = 0; i < preparedMessage.Length; i++)
             {
-                var sourceCharacter = preparedMessage[i];
+                var messageCharacter = preparedMessage[i];
                 var keyCharacter = preparedKey[i % preparedKey.Length];
                 
-                var sourceCharacterIndexInAlphabet = _alphabet.IndexOf(sourceCharacter);
+                var messageCharacterIndexInAlphabet = _alphabet.IndexOf(messageCharacter);
                 var keyCharacterIndexInAlphabet = _alphabet.IndexOf(keyCharacter);
 
-                var cipherCharacterIndexInAlphabet = (sourceCharacterIndexInAlphabet + keyCharacterIndexInAlphabet) % _alphabet.Length;
+                var cipherCharacterIndexInAlphabet = (messageCharacterIndexInAlphabet + keyCharacterIndexInAlphabet) % _alphabet.Length;
                 var cipherCharacter = _alphabet[cipherCharacterIndexInAlphabet];
                 cipher.Append(cipherCharacter);
             }
@@ -32,7 +34,23 @@ namespace VigenereCipher.Service
 
         public string Decrypt(string cipher, string key)
         {
-            return "decryption message";
+            var preparedCipher = prepareTextBeforeEncrypt(cipher);
+            var preparedKey = prepareTextBeforeEncrypt(key);
+
+            var message = new StringBuilder();
+            for (var i = 0; i < preparedCipher.Length; i++)
+            {
+                var cipherCharacter = preparedCipher[i];
+                var keyCharacter = preparedKey[i % preparedKey.Length];
+
+                var cipherCharacterIndexInAlphabet = _alphabet.IndexOf(cipherCharacter);
+                var keyCharacterIndexInAlphabet = _alphabet.IndexOf(keyCharacter);
+
+                var messageCharacterIndexInAlphabet = (cipherCharacterIndexInAlphabet - keyCharacterIndexInAlphabet + _alphabet.Length) % _alphabet.Length;
+                var messageCharacter = _alphabet[messageCharacterIndexInAlphabet];
+                message.Append(messageCharacter);
+            }
+            return message.ToString();
         }
 
         public (string message, string key) Hack(string cipher)
